b7337cf5c900062ae359554a0bc4c92d2b33e1e5
Francesc <francescq@gmail.com>
2018-04-19 16:05:06 +0200

    add checkboxwithlabel on main app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import CheckboxWithLabel from './CheckboxWithLabel';
+
 import './App.css';
 
 class App extends Component {
@@ -10,6 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <CheckboxWithLabel labelOn="On" labelOff="Off" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
+import CheckboxWithLabel from './CheckboxWithLabel';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
